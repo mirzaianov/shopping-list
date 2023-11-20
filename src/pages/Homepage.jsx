9e4edf2f0ad449a2dd939fd6aeb3a4686f339aaa
msejak <7o.mseJak@gmail.com>
2023-11-20 18:54:32 +0300

    fate: added CRUD The app now can create, read, update & delete item in the database

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,13 +1,34 @@
+import { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { signOut, onAuthStateChanged } from 'firebase/auth';
-import { auth } from '/firebase';
-import { useEffect } from 'react';
+import { auth, db } from '/firebase';
+import { uid } from 'uid';
+import { set, ref, onValue, remove, update } from 'firebase/database';
 
 export default function Homepage() {
+  const [todo, setTodo] = useState('');
+  const [todos, setTodos] = useState([]);
+  const [isEdit, setIsEdit] = useState(false);
+  const [tempUidd, setTempUidd] = useState('');
+
   const navigate = useNavigate();
 
   useEffect(() => {
     auth.onAuthStateChanged((user) => {
+      if (user) {
+        // read
+        onValue(ref(db, `/${auth.currentUser.uid}`), (snapshot) => {
+          setTodos([]);
+
+          const data = snapshot.val();
+
+          if (data !== null) {
+            Object.values(data).map((item) => {
+              setTodos((oldArray) => [...oldArray, item]);
+            });
+          }
+        });
+      }
       if (!user) {
         navigate('/');
       }
@@ -24,9 +45,57 @@ export default function Homepage() {
       });
   };
 
+  // add
+  const writeToDatabase = () => {
+    const uidd = uid();
+    set(ref(db, `/${auth.currentUser.uid}/${uidd}`), { todo, uidd });
+    setTodo('');
+  };
+
+  // update
+  const handleUpdate = (todo) => {
+    setIsEdit(true);
+    setTodo(todo.todo);
+    setTempUidd(todo.uidd);
+  };
+
+  const handleEditConfirm = () => {
+    update(ref(db, `/${auth.currentUser.uid}/${tempUidd}`), { todo, tempUidd });
+    setTodo('');
+  };
+
+  // delete
+
+  const handleDelete = (uid) => {
+    remove(ref(db, `/${auth.currentUser.uid}/${uid}`));
+  };
+
   return (
     <div className="homepage">
-      <h1>test</h1>
+      <input
+        type="text"
+        placeholder="Add Todo..."
+        value={todo}
+        onChange={(e) => {
+          setTodo(e.target.value);
+        }}
+      />
+      {todos.map((item) => (
+        <>
+          <h1>{item.todo}</h1>
+          <button onClick={() => handleUpdate(item)}>update</button>
+          <button onClick={() => handleDelete(item.uidd)}>delete</button>
+        </>
+      ))}
+      {isEdit ? (
+        <div>
+          <button onClick={handleEditConfirm}>Confirm</button>
+        </div>
+      ) : (
+        <div>
+          <button onClick={writeToDatabase}>Add</button>
+        </div>
+      )}
       <button onClick={handleSignOut}>Sign Out</button>
     </div>
   );
