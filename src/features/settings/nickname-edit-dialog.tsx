37ef7626b29f8911717d5eeccf38f907d0589fb1
Riaz Mirzaianov <87698216+mirzaianov@users.noreply.github.com>
2026-07-15 19:02:56 +0300

    fix(ATE-27): Widen settings edit button

--- src/features/settings/nickname-edit-dialog.tsx
+++ src/features/settings/nickname-edit-dialog.tsx
@@ -70,7 +70,7 @@ export default function NicknameEditDialog({ currentNickname }: NicknameEditDial
       <Dialog.Trigger
         className={clsx(
           buttonStyles.button,
-          buttonStyles.action,
+          buttonStyles.standard,
           buttonStyles.neutral,
           styles.editButton,
         )}
